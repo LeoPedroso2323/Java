7d96fd07057d977d851f8a75554dd2c3f76a4250
Leonardo <leonardopedroso857@gmail.com>
2024-07-31 11:33:20 -0300

    exdericios

---
+++ atividade1.js
@@ -0,0 +1,8 @@
+var age = 7;
+const idade = 7;
+if(idade < 18){
+  console.log("a pessoa é de menor")
+}
+else{
+console.log("a pessoa é de maior")
+}
---
+++ atividade3.js
@@ -0,0 +1,11 @@
+const nota = 7;
+
+if(nota >=0 && nota <=3){
+    console.log("o aluno foi reprovado")
+}
+if(nota >=4 && nota <=5){
+    console.log("o aluno está de recuperação")
+}
+if(nota >=6 && nota <=10){
+    console.log("o aluno foi aprovado")
+}
